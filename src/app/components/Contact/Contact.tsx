a4d55c4cef1584efc188889ac294aaf324a7926e
dannypellatt <danielpellatt92@gmail.com>
2024-06-20 14:05:25 -0700

    replaced icon img with Image

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ContactSection: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ const ContactSection: React.FC = () => {
                 <h1 className='text-3xl md:text-5xl text-white font-semibold'>Get in touch.</h1>
                 <div className="w-16 mt-12">
                     <a href="https://www.linkedin.com/in/dannypellatt/" target="_blank">
-                        <img src='/linkedin.jpeg' alt='LinkedIn Logo' className='mt-8 border border-white rounded-xl hover:opacity-70'/>                    </a>
+                        <Image src='/linkedin.jpeg' alt='LinkedIn Logo' className='mt-8 border border-white rounded-xl hover:opacity-70'/>                    </a>
                 </div>
                 <p className='py-12 text-zinc-500'>Website made by me. Designed in <span className='text-zinc-400'>Figma & Canva</span>. Coded using <span className='text-zinc-400'>TypeScript</span>, <span className='text-zinc-400'>JavaScript</span>, <span className='text-zinc-400'>HTML5</span>, and <span className='text-zinc-400'>CSS</span> in Visual Studio Code. Built with <span className='text-zinc-400'>React</span>, <span className='text-zinc-400'>Next.js</span> and <span className='text-zinc-400'>Tailwind CSS</span>.</p>
             </div>
